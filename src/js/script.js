05bba2801adad14d9eff10bd8b4b512d0d0a48fd
suarnaya <kedusashram99@gmail.com>
2022-07-24 12:39:59 +0800

    create landing page

---
+++ src/js/script.js
@@ -0,0 +1,40 @@
+// fungsi klik nav
+const navToggle = document.querySelector('.nav-toggle')
+const navItem = document.querySelector('.nav-item')
+const navUser = document.querySelector('.user')
+
+navToggle.addEventListener('click', () => {
+  navItem.classList.toggle('active')
+  navUser.classList.toggle('active')
+})
+
+// fungsi gullir produk
+const leftBtn = document.querySelector('#left-btn')
+const rightBtn = document.querySelector('#right-btn')
+const container = document.querySelector('.product-wrapper')
+const scrollField = document.querySelector('.product-scroll-wrapper')
+
+let maxScroll = -scrollField.offsetWidth + container.offsetWidth
+let scrollAmount = 800
+let currentPosition = 0
+
+function scrollHorizontally(val) {
+  currentPosition += val * scrollAmount
+
+  if (currentPosition >= 0) {
+    currentPosition = 0
+  }
+
+  if (currentPosition <= maxScroll) {
+    currentPosition = maxScroll
+  }
+  scrollField.style.left = currentPosition + 'px'
+}
+
+leftBtn.addEventListener('click', () => {
+  scrollHorizontally(1)
+})
+
+rightBtn.addEventListener('click', () => {
+  scrollHorizontally(-1)
+})

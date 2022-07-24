7cc455e4e859c10c8f61baea025f0768a50d3a18
suarnaya <kedusashram99@gmail.com>
2022-07-24 16:10:28 +0800

    fix scrollamount

--- src/js/script.js
+++ src/js/script.js
@@ -1,40 +1,37 @@
 // fungsi klik nav
-const navToggle = document.querySelector('.nav-toggle')
-const navItem = document.querySelector('.nav-item')
-const navUser = document.querySelector('.user')
+const navToggle = document.querySelector(".nav-toggle")
+const navItem = document.querySelector(".nav-item")
+const navUser = document.querySelector(".user")
 
-navToggle.addEventListener('click', () => {
-  navItem.classList.toggle('active')
-  navUser.classList.toggle('active')
+navToggle.addEventListener("click", () => {
+  navItem.classList.toggle("active")
+  navUser.classList.toggle("active")
 })
 
 // fungsi gullir produk
-const leftBtn = document.querySelector('#left-btn')
-const rightBtn = document.querySelector('#right-btn')
-const container = document.querySelector('.product-wrapper')
-const scrollField = document.querySelector('.product-scroll-wrapper')
+const leftBtn = document.querySelector("#left-btn")
+const rightBtn = document.querySelector("#right-btn")
+const container = document.querySelector(".product-wrapper")
+const scrollField = document.querySelector(".product-scroll-wrapper")
 
 let maxScroll = -scrollField.offsetWidth + container.offsetWidth
-let scrollAmount = 800
+let scrollAmount = document.querySelectorAll(".product-list")[0]
 let currentPosition = 0
 
 function scrollHorizontally(val) {
-  currentPosition += val * scrollAmount
+  currentPosition += (val * scrollAmount.offsetWidth * 10) / 9
 
-  if (currentPosition >= 0) {
-    currentPosition = 0
-  }
+  if (currentPosition >= 0) currentPosition = 0
 
-  if (currentPosition <= maxScroll) {
-    currentPosition = maxScroll
-  }
-  scrollField.style.left = currentPosition + 'px'
+  if (currentPosition <= maxScroll) currentPosition = maxScroll
+
+  scrollField.style.left = currentPosition + "px"
 }
 
-leftBtn.addEventListener('click', () => {
+leftBtn.addEventListener("click", () => {
   scrollHorizontally(1)
 })
 
-rightBtn.addEventListener('click', () => {
+rightBtn.addEventListener("click", () => {
   scrollHorizontally(-1)
 })

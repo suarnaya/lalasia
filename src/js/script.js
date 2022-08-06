d646138065cffd693d93a385fd9e340b3455de1e
suarnaya <kedusashram99@gmail.com>
2022-08-06 21:54:47 +0800

    fix size

--- src/js/script.js
+++ src/js/script.js
@@ -1,37 +1,37 @@
 // fungsi klik nav
-const navToggle = document.querySelector(".nav-toggle")
-const navItem = document.querySelector(".nav-item")
-const navUser = document.querySelector(".user")
+const navToggle = document.querySelector('.nav-toggle');
+const navItem = document.querySelector('.nav-item');
+const navUser = document.querySelector('.user');
 
-navToggle.addEventListener("click", () => {
-  navItem.classList.toggle("active")
-  navUser.classList.toggle("active")
-})
+navToggle.addEventListener('click', () => {
+    navItem.classList.toggle('active');
+    navUser.classList.toggle('active');
+});
 
 // fungsi gullir produk
-const leftBtn = document.querySelector("#left-btn")
-const rightBtn = document.querySelector("#right-btn")
-const container = document.querySelector(".product-wrapper")
-const scrollField = document.querySelector(".product-scroll-wrapper")
+const leftBtn = document.querySelector('#left-btn');
+const rightBtn = document.querySelector('#right-btn');
+const container = document.querySelector('.product-wrapper');
+const scrollField = document.querySelector('.product-scroll-wrapper');
 
-let maxScroll = -scrollField.offsetWidth + container.offsetWidth
-let scrollAmount = document.querySelectorAll(".product-list")[0]
-let currentPosition = 0
+let maxScroll = -scrollField.offsetWidth + container.offsetWidth;
+let scrollAmount = document.querySelectorAll('.product-list')[0];
+let currentPosition = 0;
 
 function scrollHorizontally(val) {
-  currentPosition += (val * scrollAmount.offsetWidth * 10) / 9
+    currentPosition += (val * scrollAmount.offsetWidth * 10) / 9;
 
-  if (currentPosition >= 0) currentPosition = 0
+    if (currentPosition >= 0) currentPosition = 0;
 
-  if (currentPosition <= maxScroll) currentPosition = maxScroll
+    if (currentPosition <= maxScroll) currentPosition = maxScroll;
 
-  scrollField.style.left = currentPosition + "px"
+    scrollField.style.left = currentPosition + 'px';
 }
 
-leftBtn.addEventListener("click", () => {
-  scrollHorizontally(1)
-})
+leftBtn.addEventListener('click', () => {
+    scrollHorizontally(1);
+});
 
-rightBtn.addEventListener("click", () => {
-  scrollHorizontally(-1)
-})
+rightBtn.addEventListener('click', () => {
+    scrollHorizontally(-1);
+});
